5e465730c5ff7e8edbfd5ab966b87cc177c7732b
Julien Dubois <julien.dubois@gmail.com>
2016-11-07 10:14:10 +0100

    initial commit

---
+++ src/main/java/io/github/jhipster/store/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package io.github.jhipster.store.config.audit;
---
+++ src/main/java/io/github/jhipster/store/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package io.github.jhipster.store.repository;
+
+import io.github.jhipster.store.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
